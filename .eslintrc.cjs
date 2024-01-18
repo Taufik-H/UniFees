92795bb680804b403151970a4c763f3aeba0b779
Taufik-H <taufikrpl1@gmail.com>
2024-01-18 19:06:01 +0700

    Add "@typescript-eslint/unbound-method" rule to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,12 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/unbound-method": [
+      "error",
+      {
+        ignoreStatic: true,
+      },
+    ],
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     // unexpected
