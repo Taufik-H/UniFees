03a0f094ff9b450816c765eedb19f8740ee1f8dc
Taufik-H <taufikrpl1@gmail.com>
2024-01-18 18:24:19 +0700

    Add use-debounce library for search functionality

--- src/app/_components/ui/searchreport.tsx
+++ src/app/_components/ui/searchreport.tsx
@@ -3,23 +3,24 @@ import { LuMapPin } from "react-icons/lu";
 import { Input } from "./input";
 import { Button } from "./button";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
 const SearchReport = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
 
-  function handleSearch() {
-    const params = new URLSearchParams(searchTerm);
-    if (searchTerm) {
-      params.set("query", searchTerm);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <>
@@ -31,13 +32,13 @@ const SearchReport = () => {
 
         <Input
           onChange={(e) => {
-            setSearchTerm(e.target.value);
+            handleSearch(e.target.value);
           }}
           defaultValue={searchParams.get("query")?.toString()}
           placeholder="Tulis lokasimu"
           className="focus-visible:ring-none text-md border-none bg-transparent text-slate-900 outline-0 placeholder:text-slate-900 focus:text-slate-900  focus:ring-transparent"
         />
-        <Button onClick={() => handleSearch()}>Cari</Button>
+        <Button>Cari</Button>
       </div>
     </>
   );
