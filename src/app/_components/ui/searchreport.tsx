6b09dee7ff8e1bd80bd722aa003ea95a1c166a66
Taufik-H <taufikrpl1@gmail.com>
2024-01-19 00:23:51 +0700

    Refactor search functionality in SearchReport component

--- src/app/_components/ui/searchreport.tsx
+++ src/app/_components/ui/searchreport.tsx
@@ -2,24 +2,19 @@
 import { LuMapPin } from "react-icons/lu";
 import { Input } from "./input";
 import { Button } from "./button";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 export default function SearchReport() {
-  const { replace } = useRouter();
-  const searchParams = useSearchParams();
+  const params = useSearchParams();
   const pathname = usePathname();
-  const [term, setTerm] = useState<string>("");
-  const [type, setType] = useState<string>("");
+  const router = useRouter();
 
-  const params = new URLSearchParams(searchParams);
-  if (term) {
-    params.set("query", term);
-  } else {
-    params.delete("query");
-  }
-  replace(`${pathname}?${params.toString()}`);
-
+  const handleSearch = (value: string) => {
+    const newParams = new URLSearchParams(params.toString());
+    newParams.set("query", value);
+    router.push(`${pathname}?${newParams.toString()}`);
+  };
   return (
     <>
       <div className="mx-auto flex  w-10/12 items-center justify-center rounded-xl bg-white p-2 focus-within:text-rose-500 md:w-full md:p-3">
@@ -29,11 +24,11 @@ export default function SearchReport() {
         />
 
         <Input
-          onChange={(e) => setType(e.target.value)}
+          onChange={(e) => handleSearch(e.target.value)}
           placeholder="Tulis lokasimu"
           className="focus-visible:ring-none text-medium text-md border-none bg-transparent text-slate-900 outline-0 placeholder:text-slate-900 focus:text-slate-900  focus:ring-transparent"
         />
-        <Button onClick={() => setTerm(type)}>Cari</Button>
+        {/* <Button onClick={() => setTerm(type)}>Cari</Button> */}
       </div>
     </>
   );
